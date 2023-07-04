26302154134c803fc4c04d8168af660fcd49b481
SPiKiC0de <momchilparpulev20a@gmail.com>
2023-07-04 13:29:55 +0300

    Alpacas are better Halo

--- src/training/day2/Homework.java
+++ src/training/day2/Homework.java
@@ -1,10 +1,12 @@
 package training.day2;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 //Momchil
 public class Homework {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         //zad.1
         /*
         Scanner scanner = new Scanner(System.in);
@@ -16,7 +18,24 @@ public class Homework {
         }
         */
         //zad.2
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter how many numbers would you like to sum:");
+        int range = scanner.nextInt();
+        int[] array = new int[range];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Enter number{" + i + "}:");
+            array[i] = scanner.nextInt();
+        }
+        int sum = 0;
+        for (int number : array) {
+            sum += number;
+        }
+        System.out.print(sum);
+*/
         //zad.3
+        /*
         List<String> words = new ArrayList<String>();
         System.out.print("Enter 4 words:");
         Scanner scanner = new Scanner(System.in);
@@ -30,6 +49,7 @@ public class Homework {
             System.out.println(word);
         }
         scanner.close();
+         */
     }
 
 }
