19c7b8f0a797d8fec9565afe6852c297afb361db
SPiKiC0de <momchilparpulev20a@gmail.com>
2023-07-04 10:35:22 +0300

    Alpacas are better Halo

---
+++ src/training/day2/Main.java
@@ -0,0 +1,43 @@
+package training.day2;
+
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //zad.1
+        /*
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Register your name:");
+        String name = scanner.next();
+
+        System.out.print("Register your password:");
+        String pass = scanner.next();
+        System.out.print("Registered successfully!");
+        Scanner scanner1 = new Scanner(System.in);
+
+        System.out.print("Log in with your name:");
+        String namechecker = scanner1.next();
+
+        System.out.print("Log in with your password:");
+        String passcheck = scanner1.next();
+
+        if (name.equals(namechecker)&&pass.equals(passcheck))
+        {
+                System.out.println("Welcome, "+name);
+
+        }else{
+            System.out.println("Wrong credentials!");
+        }
+
+         */
+        //zad.2
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your word:");
+        String word = scanner.next();
+
+        
+        }
+}
