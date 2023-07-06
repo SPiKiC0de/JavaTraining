f7233fac3a3063f26f48f43260c0adc983948ebc
SPiKiC0de <momchilparpulev20a@gmail.com>
2023-07-06 11:44:29 +0300

    Aloha

---
+++ src/training/day4/Car.java
@@ -0,0 +1,8 @@
+package training.day4;
+
+public class Car extends Vehicle{
+    private final double DEFAULT_FUEL_CONSUMPTION = 3;
+    public Car(double fuel, int hp){
+        super(fuel,hp);
+    }
+}
---
+++ src/training/day4/SportCar.java
@@ -0,0 +1,8 @@
+package training.day4;
+
+public class SportCar extends Vehicle{
+     private final double DEFAULT_FUEL_CONSUMPTION = 10;
+    public SportCar(double fuel, int hp) {
+        super(fuel, hp);
+    }
+}
