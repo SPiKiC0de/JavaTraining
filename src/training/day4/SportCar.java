9a44a2e0d786c5e0887dabbd8173f7d19d910bde
SPiKiC0de <momchilparpulev20a@gmail.com>
2023-07-12 10:57:44 +0300

    Week2_Day1

--- src/training/day4/SportCar.java
+++
@@ -1,8 +0,0 @@
-package training.day4;
-
-public class SportCar extends Vehicle{
-    private final double DEFAULT_FUEL_CONSUMPTION = 10;
-    public SportCar(double fuel, int hp) {
-        super(fuel, hp);
-    }
-}
